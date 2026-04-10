1cd94fa03ee01fdd20448209c12a4d37d3ee17ea
Stephaneie Kanali <stephaniekanali0@gmail.com>
2026-04-10 08:51:55 +0300

    I have used Modules and syntax to create a function to merge the array of users

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+const combinedObject=combineUsers(["_.mjaka.mfine","allanmax","sylexloveu","shee.actress","kanash_steffy","urfav._kieti"])
 
+const combinedObject1=combineUsers(["lilexis_5","farries_megaverse","koshie_lay_lay","vibes_babes_","Life.with.amor","michy_.k"])
 
+const combinedObject2=combineUsers()
+
+let new_combine=["...combine","lilexis_5","farries_megaverse","koshie_lay_lay","vibes_babes_","Life.with.amor","michy_.k"]
+
+console.log(new_combine)
+
+ const combinedObject3.merge_date=[04/10/2025];
+
+return combinedObject()
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
