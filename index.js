436fed2b084984e6b9bf7bc4f496d8fe3b8f80bd
Stephaneie Kanali <stephaniekanali0@gmail.com>
2026-04-14 10:56:03 +0300

    Refactor combineUsers function to improve user merging logicand updated the package name

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-const combinedObject=combineUsers(["_.mjaka.mfine","allanmax","sylexloveu","shee.actress","kanash_steffy","urfav._kieti"])
+function combineUsers (...args){
 
-const combinedObject1=combineUsers(["lilexis_5","farries_megaverse","koshie_lay_lay","vibes_babes_","Life.with.amor","michy_.k"])
+ let combinedObject={
+  users: []
+ }
 
-const combinedObject2=combineUsers()
+ for(let arry of args){
+  combinedObject.users = [...combinedObject.users,...arry]
+  }
 
-let new_combine=["...combine","lilexis_5","farries_megaverse","koshie_lay_lay","vibes_babes_","Life.with.amor","michy_.k"]
+  combinedObject.merge_date = new Date().toString("M/d/yyyy")
 
-console.log(new_combine)
+  return combinedObject;
 
- const combinedObject3.merge_date=[04/10/2025];
-
-return combinedObject()
-
+}
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
